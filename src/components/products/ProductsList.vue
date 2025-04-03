95660ae852628389cc79e67e6217157c98bc7c55
sidhanth-ilg <162859143+sidhanth-ilg@users.noreply.github.com>
2025-04-03 21:01:44 +0530

    add @vueuse/core for virtual list implementation in ProductsList component

--- src/components/products/ProductsList.vue
+++ src/components/products/ProductsList.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
 import useProductsList from '@/components/products/composables/useProductsList'
 import ProductItem from '@/components/products/ProductItem.vue'
+import { useVirtualList } from '@vueuse/core'
 
 const { products } = useProductsList()
+
+const { list, containerProps, wrapperProps } = useVirtualList(products, {
+  itemHeight: 384,
+  overscan: 5,
+})
 </script>
 
 <template>
-  <ProductItem v-for="product in products" :key="product.id" :product="product" />
+  <div v-bind="containerProps" class="virtual-list-container">
+    <div v-bind="wrapperProps">
+      <ProductItem v-for="item in list" :key="item.data.id" :product="item.data" />
+    </div>
+  </div>
 </template>
+<style scoped>
+.virtual-list-container {
+  height: calc(100vh - 120px);
+  overflow-y: auto;
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+</style>
