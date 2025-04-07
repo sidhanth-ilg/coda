f3403a30049ffeec2329680e190446ce36f7f993
sidhanth-ilg <162859143+sidhanth-ilg@users.noreply.github.com>
2025-04-07 21:28:13 +0530

    refactor: add conditional rendering for product list and improve empty state message

--- src/components/products/ProductsList.vue
+++ src/components/products/ProductsList.vue
@@ -7,6 +7,19 @@ const { products } = useProductsList()
 
 <template>
   <div data-testid="product-list">
-    <ProductItem v-for="product in products" :key="product.id" :product="product" />
+    <template v-if="products.length">
+      <ProductItem v-for="product in products" :key="product.id" :product="product" />
+    </template>
+    <div v-else>
+      <p class="products-list__empty-text">Sorry ! No products found.</p>
+    </div>
   </div>
 </template>
+<style scoped>
+.products-list__empty-text {
+  color: white;
+  font-size: 1.5rem;
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>
